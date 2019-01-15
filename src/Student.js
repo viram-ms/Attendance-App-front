4f2f778d6d58b7da325446b5b6c63160d14d8090
viram-ms <viram1999@gmail.com>
2019-01-15 11:09:46 +0530

    initial setUp

---
+++ src/Student.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PersistentDrawerLeft from './Components/PersistentDrawerLeft';
+
+
+class Student extends React.Component{
+    render(){
+        return(
+            <div><h1>Student</h1></div>
+        );
+    }
+}
+
+export default Student;
