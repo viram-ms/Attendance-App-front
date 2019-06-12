0da1620f70a40b7ec77bf71b9c7c37c7976ec9bf
viram-ms <viram1999@gmail.com>
2019-06-13 01:21:30 +0530

    Updates

---
+++ precache-manifest.994d0e9affb58bf15464c1b9666ff895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a599682035708dd20fe",
+    "url": "/Attendance-front/static/js/runtime~main.7a599682.js"
+  },
+  {
+    "revision": "6dd836517f7caad4a8d0",
+    "url": "/Attendance-front/static/js/main.6dd83651.chunk.js"
+  },
+  {
+    "revision": "65fd49d581d3fcafe8d5",
+    "url": "/Attendance-front/static/js/1.65fd49d5.chunk.js"
+  },
+  {
+    "revision": "6dd836517f7caad4a8d0",
+    "url": "/Attendance-front/static/css/main.e016f647.chunk.css"
+  },
+  {
+    "revision": "65fd49d581d3fcafe8d5",
+    "url": "/Attendance-front/static/css/1.015ec344.chunk.css"
+  },
+  {
+    "revision": "6fa0fba906cfb05aa7b943fedd8d12de",
+    "url": "/Attendance-front/index.html"
+  }
+];
